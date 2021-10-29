9e7ce30a2b0db685ee5491d9438a29725fdfc399
Morgan Benton <morgan.benton@gmail.com>
2021-10-29 13:52:34 -0400

    created basic game skeleton

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Classic snake game in python. Code was generated following the
+tutorial at: https://www.edureka.co/blog/snake-game-with-pygame/
+"""
+
+# import required packages
+import pygame
+
+# initialize the game
+pygame.init()
+
+# set the screen size
+dis = pygame.display.set_mode((400, 300))
+
+# draw the screen
+pygame.display.update()
+
+# quit the game
+pygame.quit()
+quit()
